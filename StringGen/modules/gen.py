cd898df9e61886f7b170b25425c14fd249bb6709
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-09 23:06:47 +0700

    Update gen.py

--- StringGen/modules/gen.py
+++ StringGen/modules/gen.py
@@ -51,14 +51,14 @@ async def gen_session(
     try:
         api_id = await Anony.ask(
             identifier=(message.chat.id, user_id, None),
-            text="» ᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ʏᴏᴜʀ ᴀᴘɪ ɪᴅ ᴛᴏ ᴘʀᴏᴄᴇᴇᴅ :",
+            text="» ᴋɪʀɪᴍ sɪɴɪ ᴀᴘɪ ɪᴅ ʟᴜ ᴋᴀᴋ :",
             filters=filters.text,
             timeout=300,
         )
     except ListenerTimeout:
         return await Anony.send_message(
             user_id,
-            "» ᴛɪᴍᴇᴅ ʟɪᴍɪᴛ ʀᴇᴀᴄʜᴇᴅ ᴏғ 5 ᴍɪɴᴜᴛᴇs.\n\nᴘʟᴇᴀsᴇ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ sᴇssɪᴏɴ ᴀɢᴀɪɴ.",
+            "» ʙᴀᴛᴀs ᴡᴀᴋᴛᴜ ʟᴜ ᴜᴅᴀʜ ʜᴀʙɪs ᴋᴀᴋ.\n\nᴄᴏʙᴀ sᴛᴀʀᴛ ᴜʟᴀɴɢ.",
             reply_markup=retry_key,
         )
 
@@ -70,21 +70,21 @@ async def gen_session(
     except ValueError:
         return await Anony.send_message(
             user_id,
-            "» ᴛʜᴇ ᴀᴘɪ ɪᴅ ʏᴏᴜ'ᴠᴇ sᴇɴᴛ ɪs ɪɴᴠᴀʟɪᴅ.\n\nᴘʟᴇᴀsᴇ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ sᴇssɪᴏɴ ᴀɢᴀɪɴ.",
+            "» ᴀᴘɪ ɪᴅ ʏᴀɴɢ ʟᴜ ᴋɪʀɪᴍ sᴀʟᴀʜ ᴄᴏʙᴀ ᴄᴇᴋ ᴅᴜʟᴜ.\n\nʙᴀʀᴜ sᴛᴀʀᴛ ʟᴀɢɪ.",
             reply_markup=retry_key,
         )
 
     try:
         api_hash = await Anony.ask(
             identifier=(message.chat.id, user_id, None),
-            text="» ᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ʏᴏᴜʀ ᴀᴘɪ ʜᴀsʜ ᴛᴏ ᴘʀᴏᴄᴇᴇᴅ :",
+            text="» ᴋɪʀɪᴍ sɪɴɪ ᴀᴘɪ ʜᴀsʜ ʟᴜ ᴋᴀᴋ :",
             filters=filters.text,
             timeout=300,
         )
     except ListenerTimeout:
         return await Anony.send_message(
             user_id,
-            "» ᴛɪᴍᴇᴅ ʟɪᴍɪᴛ ʀᴇᴀᴄʜᴇᴅ ᴏғ 5 ᴍɪɴᴜᴛᴇs.\n\nᴘʟᴇᴀsᴇ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ sᴇssɪᴏɴ ᴀɢᴀɪɴ.",
+            "» ʙᴀᴛᴀs ᴡᴀᴋᴛᴜ ʟᴜ ᴜᴅᴀʜ ʜᴀʙɪs ᴋᴀᴋ. \n\nᴄᴏʙᴀ sᴛᴀʀᴛ ᴜʟᴀɴɢ.",
             reply_markup=retry_key,
         )
 
@@ -96,21 +96,21 @@ async def gen_session(
     if len(api_hash) < 30:
         return await Anony.send_message(
             user_id,
-            "» ᴛʜᴇ ᴀᴘɪ ʜᴀsʜ ʏᴏᴜ'ᴠᴇ sᴇɴᴛ ɪs ɪɴᴠᴀʟɪᴅ.\n\nᴘʟᴇᴀsᴇ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ sᴇssɪᴏɴ ᴀɢᴀɪɴ.",
+            "» ᴀᴘɪ ʜᴀsʜ ʏᴀɴɢ ʟᴜ ᴋɪʀɪᴍ sᴀʟᴀʜ ᴄᴏʙᴀ ᴄᴇᴋ ᴅᴜʟᴜ.\n\nʙᴀʀᴜ sᴛᴀʀᴛ ʟᴀɢɪ.",
             reply_markup=retry_key,
         )
 
     try:
         phone_number = await Anony.ask(
             identifier=(message.chat.id, user_id, None),
-            text="» ᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ʏᴏᴜʀ ᴘʜᴏɴᴇ ɴᴜᴍʙᴇʀ ᴛᴏ ᴘʀᴏᴄᴇᴇᴅ :",
+            text="» ᴍᴀɴᴀ ɴᴏᴍᴏʀ ᴀᴋᴜɴ ɴʏᴀ ᴀɴᴊ ɴɢᴀɴᴛᴜᴋ ɴɪ ɢᴡ :",
             filters=filters.text,
             timeout=300,
         )
     except ListenerTimeout:
         return await Anony.send_message(
             user_id,
-            "» ᴛɪᴍᴇᴅ ʟɪᴍɪᴛ ʀᴇᴀᴄʜᴇᴅ ᴏғ 5 ᴍɪɴᴜᴛᴇs.\n\nᴘʟᴇᴀsᴇ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ sᴇssɪᴏɴ ᴀɢᴀɪɴ.",
+            "» ʙᴀᴛᴀs ᴡᴀᴋᴛᴜ ʟᴜ ᴜᴅᴀʜ ʜᴀʙɪs ᴋᴀᴋ.\n\nᴄᴏʙᴀ sᴛᴀʀᴛ ᴜʟᴀɴɢ.",
             reply_markup=retry_key,
         )
 
@@ -137,26 +137,26 @@ async def gen_session(
     except FloodWait as f:
         return await Anony.send_message(
             user_id,
-            f"» ғᴀɪʟᴇᴅ ᴛᴏ sᴇɴᴅ ᴄᴏᴅᴇ ғᴏʀ ʟᴏɢɪɴ.\n\nᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ғᴏʀ {f.value or f.x} sᴇᴄᴏɴᴅs ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ.",
+            f"» ɢᴀɢᴀʟ ᴍᴇɴɢɪʀɪᴍ ᴄᴏᴅᴇ ʟᴏɢɪɴ.\n\nᴛᴜɴɢɢᴜ ʙᴇɴᴛᴀʀᴀɴ {f.value or f.x} ᴄᴏʙᴀ ʟᴀɢɪ ɴᴀɴᴛɪ.",
             reply_markup=retry_key,
         )
     except (ApiIdInvalid, ApiIdInvalidError, ApiIdInvalid1):
         return await Anony.send_message(
             user_id,
-            "» ᴀᴘɪ ɪᴅ ᴏʀ ᴀᴘɪ ʜᴀsʜ ɪs ɪɴᴠᴀʟɪᴅ.\n\nᴘʟᴇᴀsᴇ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ ʏᴏᴜʀ sᴇssɪᴏɴ ᴀɢᴀɪɴ.",
+            "» ᴀᴘɪ ɪᴅ ʜᴀsʜ ʏᴀɴɢ ʟᴜ ᴋɪʀɪᴍ sᴀʟᴀʜ ᴄᴏʙᴀ ᴄᴇᴋ ᴅᴜʟᴜ.\n\nᴄᴏʙᴀ sᴛᴀʀᴛ ʟᴀɢɪ.",
             reply_markup=retry_key,
         )
     except (PhoneNumberInvalid, PhoneNumberInvalidError, PhoneNumberInvalid1):
         return await Anony.send_message(
             user_id,
-            "» ᴘʜᴏɴᴇ ɴᴜᴍʙᴇʀ ɪɴᴠᴀʟɪᴅ.\n\nᴘʟᴇᴀsᴇ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ ʏᴏᴜʀ sᴇssɪᴏɴ ᴀɢᴀɪɴ.",
+            "» ɴᴏᴍᴏʀ ᴀᴋᴜɴ ɴʏᴀ sᴀʟᴀʜ.\n\nᴄᴏʙᴀ sᴛᴀʀᴛ ʟᴀɢɪ.",
             reply_markup=retry_key,
         )
 
     try:
         otp = await Anony.ask(
             identifier=(message.chat.id, user_id, None),
-            text=f"ᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴛʜᴇ ᴏᴛᴘ sᴇɴᴛ ᴛᴏ {phone_number}.\n\nɪғ ᴏᴛᴩ ɪs <code>12345</code>, ᴩʟᴇᴀsᴇ sᴇɴᴅ ɪᴛ ᴀs <code>1 2 3 4 5.</code>",
+            text=f"ᴋɪʀɪᴍ sɪɴɪ ᴏᴛᴘ ɴʏᴀ ᴋᴀᴋ {phone_number}.\n\nᴄᴏɴᴛᴏʜ ᴏᴛᴩ <code>12345</code>, ʟᴜ ᴋɪʀɪᴍ ɴʏᴀ ᴘᴀᴋᴇ sᴘᴀsɪ ʏᴀ ᴍᴇᴋ <code>1 2 3 4 5.</code>",
             filters=filters.text,
             timeout=600,
         )
@@ -165,7 +165,7 @@ async def gen_session(
     except ListenerTimeout:
         return await Anony.send_message(
             user_id,
-            "» ᴛɪᴍᴇ ʟɪᴍɪᴛ ʀᴇᴀᴄʜᴇᴅ ᴏғ 10 ᴍɪɴᴜᴛᴇs.\n\nᴩʟᴇᴀsᴇ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ ʏᴏᴜʀ sᴇssɪᴏɴ ᴀɢᴀɪɴ.",
+            "» ʙᴀᴛᴀs ᴡᴀᴋᴛᴜ ʟᴜ ᴜᴅᴀʜ ʜᴀʙɪs ᴋᴀᴋ.\n\nᴄᴏʙᴀ sᴛᴀʀᴛ ʟᴀɢɪ.",
             reply_markup=retry_key,
         )
 
@@ -178,13 +178,13 @@ async def gen_session(
     except (PhoneCodeInvalid, PhoneCodeInvalidError, PhoneCodeInvalid1):
         return await Anony.send_message(
             user_id,
-            "» ᴛʜᴇ ᴏᴛᴩ ʏᴏᴜ'ᴠᴇ sᴇɴᴛ ɪs <b>ᴡʀᴏɴɢ.</b>\n\nᴩʟᴇᴀsᴇ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ ʏᴏᴜʀ sᴇssɪᴏɴ ᴀɢᴀɪɴ.",
+            "» ᴏᴛᴩ ɴʏᴀ sᴀʟᴀʜ.</b>\n\nᴄᴏʙᴀ sᴛᴀʀᴛ ʟᴀɢɪ..",
             reply_markup=retry_key,
         )
     except (PhoneCodeExpired, PhoneCodeExpiredError, PhoneCodeExpired1):
         return await Anony.send_message(
             user_id,
-            "» ᴛʜᴇ ᴏᴛᴩ ʏᴏᴜ'ᴠᴇ sᴇɴᴛ ɪs <b>ᴇxᴩɪʀᴇᴅ.</b>\n\nᴩʟᴇᴀsᴇ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ ʏᴏᴜʀ sᴇssɪᴏɴ ᴀɢᴀɪɴ.",
+            "» ᴋᴇʟᴀᴍᴀᴀɴ ᴀɴᴊ ɴɢɪʀɪᴍ ᴏᴛᴘ ᴊᴜɢᴀ <b>ᴊᴀᴅɪ ɢᴀɢᴀʟ.</b>\n\nᴄᴏʙᴀ sᴛᴀʀᴛ ʟᴀɢɪ.",
             reply_markup=retry_key,
         )
     except (SessionPasswordNeeded, SessionPasswordNeededError, SessionPasswordNeeded1):
