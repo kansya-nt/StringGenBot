ed3dad545134d3c02fdea9bc167b8caf99b95839
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-30 04:11:49 +0700

    A config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv(""))
-API_HASH = getenv("")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
-BOT_TOKEN = getenv("")
-MONGO_DB_URI = getenv("")
+BOT_TOKEN = getenv("BOT_TOKEN")
+MONGO_DB_URI = getenv("MONGO_DB_URI")
 OWNER_ID = int(getenv("OWNER_ID", 1499130346))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Asupanhot_viral")
