d420c47e18752ae534a128a0bf0d632a117f6973
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-30 03:53:19 +0700

    A config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("20609170"))
+API_HASH = getenv("cd0fa6d4f7c452ec5617bb0b1e4dc479")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+BOT_TOKEN = getenv("6742240257:AAHH7RDQILs6eHQFtdXwy02eLFsuNheQsx0")
+MONGO_DB_URI = getenv("mongodb+srv://babunt1:1234@cluster0.8wb8vhj.mongodb.net/?retryWrites=true&w=majority", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 5872109995))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Berlinmusic_support")
+OWNER_ID = int(getenv("OWNER_ID", 1499130346))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Asupanhot_viral")
